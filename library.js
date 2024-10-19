b99b1ec4f4d8f62aea32a75b97f31d34d4b0ce40
rod-nickle <rod@bitwise-it.com>
2024-10-19 11:48:43 -0600

    Half way done.

--- library.js
+++ library.js
@@ -23,6 +23,7 @@ const library = {
              }
 };
 
+
 /////////////////////////////
 // FUNCTIONS TO IMPLEMENT:
 /////////////////////////////
@@ -31,7 +32,17 @@ const library = {
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
 const printPlaylists = function() {
+  let playListId;
+  let playListName;
+  let playListTrackCount;
 
+  // Loop through the keys of the playlist object.
+  for (const playList in library.playlists) {
+    playListId = library["playlists"][playList]["id"];
+    playListName = library["playlists"][playList]["name"];
+    playListTrackCount = library["playlists"][playList]["tracks"].length;
+    console.log(`${playListId}: ${playListName} - ${playListTrackCount} tracks`);
+  }
 }
 
 
@@ -40,7 +51,19 @@ const printPlaylists = function() {
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 const printTracks = function() {
+  let trackId;
+  let trackName;
+  let artist;
+  let album;
 
+  // Loop through the keys of the tracks object.
+  for (const track in library.tracks) {
+    trackId = library["tracks"][track]["id"];
+    trackName = library["tracks"][track]["name"];
+    artist = library["tracks"][track]["artist"];
+    album = library["tracks"][track]["album"];
+    console.log(`${trackId}: ${trackName} by ${artist} (${album})`);
+  }
 }
 
 
@@ -49,13 +72,36 @@ const printTracks = function() {
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 const printPlaylist = function(playlistId) {
+  let playListId;
+  let playListName;
+  let playListTrackCount;
+  let tracks = [];
+  let trackId;
+  let trackName;
+  let artist;
+  let album;
 
+  // Print the Playlist
+  playListId = library["playlists"][playlistId]["id"];
+  playListName = library["playlists"][playlistId]["name"];
+  playListTrackCount = library["playlists"][playlistId]["tracks"].length;
+  console.log(`${playListId}: ${playListName} - ${playListTrackCount} tracks`);
+
+  // Print the tracks
+  tracks = library["playlists"][playlistId]["tracks"];
+  for (let track of tracks) {
+    trackId = library["tracks"][track]["id"];
+    trackName = library["tracks"][track]["name"];
+    artist = library["tracks"][track]["artist"];
+    album = library["tracks"][track]["album"];
+    console.log(`${trackId}: ${trackName} by ${artist} (${album})`);
+  }
 }
 
 
 // adds an existing track to an existing playlist
 const addTrackToPlaylist = function(trackId, playlistId) {
-
+  library["playlists"][playlistId]["tracks"].push(trackId);
 }
 
 
@@ -68,7 +114,15 @@ const generateUid = function() {
 
 // adds a track to the library
 const addTrack = function(name, artist, album) {
+  const trackId = generateUid();
+  console.log(trackId);
+  library["tracks"] = {trackId = {
+    id: trackId,
+    name: name,
+    artist: artist,
+    album: album}};
 
+  
 }
 
 
@@ -86,3 +140,32 @@ const addPlaylist = function(name) {
 const printSearchResults = function(query) {
 
 }
+
+
+// // Print Playlists
+// console.log("--- printPlaylists function ---");
+// printPlaylists();
+
+// // Print Tracks
+// console.log("--- printTracks function ---");
+// printTracks();
+
+// // Print Playlist
+// console.log("--- printPlaylist function p01 ---");
+// printPlaylist("p01"); 
+// console.log("--- printPlaylist function p02 ---");
+// printPlaylist("p02"); 
+
+// // addTrackToPlaylist
+// console.log("--- addTrackToPlaylist function p01 ---");
+// addTrackToPlaylist("t03", "p01");
+// printPlaylist("p01"); 
+// console.log("--- addTrackToPlaylist function p02 ---");
+// addTrackToPlaylist("t02", "p02");
+// addTrackToPlaylist("t01", "p02");
+// printPlaylist("p02");
+
+// addTrack
+console.log("--- addTrack function p01 ---");
+addTrack("Thunder Road", "Bruce Springstee", "ThunderRoad");
+console.log(library["tracks"])
