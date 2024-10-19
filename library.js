df56cd1c2d5d31b45716ad42142264ae5e6eff79
rod-nickle <rod@bitwise-it.com>
2024-10-19 12:37:09 -0600

    Completed library.js

--- library.js
+++ library.js
@@ -1,26 +1,36 @@
 const library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             }
+  tracks: {
+    t01: {
+      id: "t01",
+      name: "Code Monkey",
+      artist: "Jonathan Coulton",
+      album: "Thing a Week Three"
+    },
+    t02: {
+      id: "t02",
+      name: "Model View Controller",
+      artist: "James Dempsey",
+      album: "WWDC 2003"
+    },
+    t03: {
+      id: "t03",
+      name: "Four Thirty-Three",
+      artist: "John Cage",
+      album: "Woodstock 1952"
+    }
+  },
+  playlists: {
+    p01: {
+      id: "p01",
+      name: "Coding Music",
+      tracks: ["t01", "t02"]
+    },
+    p02: {
+      id: "p02",
+      name: "Other Playlist",
+      tracks: ["t03"]
+    }
+  }
 };
 
 
@@ -43,7 +53,7 @@ const printPlaylists = function() {
     playListTrackCount = library["playlists"][playList]["tracks"].length;
     console.log(`${playListId}: ${playListName} - ${playListTrackCount} tracks`);
   }
-}
+};
 
 
 // prints a list of all tracks, using the following format:
@@ -64,7 +74,7 @@ const printTracks = function() {
     album = library["tracks"][track]["album"];
     console.log(`${trackId}: ${trackName} by ${artist} (${album})`);
   }
-}
+};
 
 
 // prints a list of tracks for a given playlist, using the following format:
@@ -96,76 +106,115 @@ const printPlaylist = function(playlistId) {
     album = library["tracks"][track]["album"];
     console.log(`${trackId}: ${trackName} by ${artist} (${album})`);
   }
-}
+};
 
 
 // adds an existing track to an existing playlist
 const addTrackToPlaylist = function(trackId, playlistId) {
   library["playlists"][playlistId]["tracks"].push(trackId);
-}
+};
 
 
 // generates a unique id
 // (already implemented: use this for addTrack and addPlaylist)
 const generateUid = function() {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-}
+};
 
 
 // adds a track to the library
 const addTrack = function(name, artist, album) {
   const trackId = generateUid();
-  console.log(trackId);
-  library["tracks"] = {trackId = {
+
+  library["tracks"][trackId] = {
     id: trackId,
     name: name,
     artist: artist,
-    album: album}};
-
-  
-}
+    album: album};
+};
 
 
 // adds a playlist to the library
 const addPlaylist = function(name) {
+  const playListId = generateUid();
 
-}
+  library["playlists"][playListId] = {
+    id: playListId,
+    name: name,
+    tracks: []};
+};
 
 
 // STRETCH:
 // given a query string string, prints a list of tracks
 // where the name, artist or album contains the query string (case insensitive)
-// tip: use "string".search("tri") 
+// tip: use "string".search("tri")
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
 const printSearchResults = function(query) {
+  let trackId;
+  let trackName;
+  let artist;
+  let album;
+  query = query.toUpperCase();
 
-}
+  // Loop through the keys of the tracks object.
+  for (const track in library.tracks) {
+    trackId = library["tracks"][track]["id"];
+    trackName = library["tracks"][track]["name"];
+    artist = library["tracks"][track]["artist"];
+    album = library["tracks"][track]["album"];
+
+    // Does the name, artist or album match the search string (case insensitive)
+    if (trackName.toUpperCase().search(query) >= 0 ||
+    artist.toUpperCase().search(query) >= 0 ||
+    album.toUpperCase().search(query) >= 0) {
+      console.log(`${trackId}: ${trackName} by ${artist} (${album})`);
+    }
+  }
+};
 
 
 // // Print Playlists
-// console.log("--- printPlaylists function ---");
+// console.log("\n--- printPlaylists function ---");
 // printPlaylists();
 
 // // Print Tracks
-// console.log("--- printTracks function ---");
+// console.log("\n--- printTracks function ---");
 // printTracks();
 
 // // Print Playlist
-// console.log("--- printPlaylist function p01 ---");
-// printPlaylist("p01"); 
-// console.log("--- printPlaylist function p02 ---");
-// printPlaylist("p02"); 
+// console.log("\n--- printPlaylist function p01 ---");
+// printPlaylist("p01");
+// console.log("\n--- printPlaylist function p02 ---");
+// printPlaylist("p02");
 
 // // addTrackToPlaylist
-// console.log("--- addTrackToPlaylist function p01 ---");
+// console.log("\n--- addTrackToPlaylist function p01 ---");
 // addTrackToPlaylist("t03", "p01");
-// printPlaylist("p01"); 
-// console.log("--- addTrackToPlaylist function p02 ---");
+// printPlaylist("p01");
+// console.log("\n--- addTrackToPlaylist function p02 ---");
 // addTrackToPlaylist("t02", "p02");
 // addTrackToPlaylist("t01", "p02");
 // printPlaylist("p02");
 
-// addTrack
-console.log("--- addTrack function p01 ---");
-addTrack("Thunder Road", "Bruce Springstee", "ThunderRoad");
-console.log(library["tracks"])
+// // addTrack
+// console.log("\n--- addTrack function ---");
+// addTrack("Thunder Road", "Bruce Springsteen", "ThunderRoad");
+// addTrack("Badlands", "Bruce Springsteen", "Darkness on the Edge of Town");
+// console.log(library["tracks"]);
+
+// // addPlaylist
+// console.log("\n--- addPlaylist function ---");
+// addPlaylist("Rod's Playlist");
+// addPlaylist("Emily's Playlist");
+// console.log(library["playlists"]);
+
+// // printSearchResults
+// console.log("\n--- printSearchResults function Three ---");
+// printSearchResults("Three");
+// console.log("\n--- printSearchResults function three---");
+// printSearchResults("three");
+// console.log("\n--- printSearchResults function THREE---");
+// printSearchResults("THREE");
+// console.log("\n--- printSearchResults function jo ---");
+// printSearchResults("jo");
